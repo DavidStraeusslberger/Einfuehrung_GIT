8dcc5f1d388191a400025e7a63dff4f25768dc42
david <dstraeus@htl-steyr.ac.at>
2020-11-11 11:47:31 +0100

    Konflikt wird erzwungen

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Raphi");
         System.out.println("Hallo");
-
+        System.out.println("https://hanime.tv/playlists/rkbgjayiwwtrgafnjqwq");
         for (int i = 0; i < 10; i++){
             System.out.println("Hallo ich winke die zu");
 
