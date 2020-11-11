0c07832631c2c3ee3f5085b070776c3398a4c276
Krapfi <rhabichl@htl-steyr.ac.at>
2020-11-11 11:40:01 +0100

    Added Halllo ich winke dir zu

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Raphi");
         System.out.println("Hallo");
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hallo ich winke die zu");
+
+        }
     }
 
 }
