dcee9d5ba471f4b55f95f3579b989be3159ff538
Krapfi <rhabichl@htl-steyr.ac.at>
2020-11-11 11:46:21 +0100

    Added Chiggy on line 6

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Raphi");
         System.out.println("Hallo");
+        System.out.println("Chiggy");
 
         for (int i = 0; i < 10; i++){
             System.out.println("Hallo ich winke die zu");
